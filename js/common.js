96cdbc714a9f3f642c90f86d9830a1f304b9def1
nukenukem4 <nukenukem@yandex.ru>
2017-03-08 03:37:06 +0300

    js

--- js/common.js
+++ js/common.js
@@ -1,7 +1,6 @@
-@@ -0,0 +1,49 @@
-var content = document.getElementsByName(".content");
+var content = document.getElementsByClassName("content");
 
-function createItem(lorem, lorem, ...) {
+function createItem() {
 	var feed = document.createElement("div");
 	feed.setAttribute("class", "feed");
 
@@ -14,37 +13,62 @@ function createItem(lorem, lorem, ...) {
     photosq.setAttribute("class", "photosq");
     
     var profilePhoto = document.createElement("img");
-    profilePhoto.setAttribute("src", "../img/girlsq.jpg");
+    profilePhoto.setAttribute("src", "img/girlsq.jpg");
     
     var profileName = document.createElement("h3");
     
     var date = document.createElement("date");
   
 	var photo = document.createElement("img");
-	img.setAttribute("src", "../img/girl.jpg");
+	photo.setAttribute("src", "img/girl.jpg");
     
     var text = document.createElement("div");
     text.setAttribute("class", "text");
     
     var Coments = document.createElement("ul");
-    coments.setAttribute("class", "coments");
-    
+    Coments.setAttribute("class", "coments");
     
     var likes = document.createElement("li");
-    like.setAttribute("id", "likes");
-    likes.textContent ="<b>" + likesNum + "likes </b>";
+    likes.setAttribute("id", "likes");
+    likes.textContent ="<b> 33 likes </b>";
     
-    var com = document.createElement("li")
+    var Com = document.createElement("li")
+    Com.textContent = "<a href='#'><b> nukenukem </b></a> Крутое фото!!! "; 
     
-    var form = document.createElement("form")
+    var form = document.createElement("form");
     
-    var like = Document.createElement("input")
-    like.setAttribute("type","checkbox")
-    like.setAttribute("id","like")
+    var like = document.createElement("input");
+    like.setAttribute("type","checkbox");
+    like.setAttribute("id","like");
     
-    var likel = document.createElement("label")
-    likel.setAttribute("for","like")
+    var likel = document.createElement("label");
+    likel.setAttribute("for","like");;
+    
+    var commentInput = document.createElement("input");
+    commentInput.setAttribute("placeholder", "Add a comment");
 
-	content.appendChild(feed);
-	
+    feed.appendChild(head);
+        head.appendChild(profile);
+            profile.appendChild(photosq);
+                photosq.appendChild(profilePhoto);
+            profile.appendChild(profileName);
+        head.appendChild(date);
+    feed.appendChild(photo);
+    feed.appendChild(text);
+        text.appendChild(Coments);
+            Coments.appendChild(likes);
+            Coments.appendChild(Com);
+    feed.appendChild(text);
+        text.appendChild(form);
+            form.appendChild(like);
+            form.appendChild(likel);
+            form.appendChild(commentInput);
+
+    return feed
+                
 };
+
+content.appendChild(createItem());
+
+
+
